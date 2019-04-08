1ec1ae01622281648a8fe7b863dd0aabe66f07ae
masterUNG <phrombutr@gmail.com>
2019-04-09 04:57:59 +0700

    Receive Token

--- lib/screens/show_news_list.dart
+++ lib/screens/show_news_list.dart
@@ -7,15 +7,47 @@ class ShowNewsList extends StatefulWidget {
 }
 
 class _ShowNewsListState extends State<ShowNewsList> {
+  String textValue = 'Show News List';
+  FirebaseMessaging firebaseMessageing = new FirebaseMessaging();
+
+  @override
+  void initState() {
+    firebaseMessageing.configure(onLaunch: (Map<String, dynamic> msg) {
+      print('onLaunch Call:');
+    }, onResume: (Map<String, dynamic> msg) {
+      print('onResume Call:');
+    }, onMessage: (Map<String, dynamic> msg) {
+      print('onMessage Call:');
+    });
+    firebaseMessageing.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, alert: true, badge: true));
+    firebaseMessageing.onIosSettingsRegistered
+        .listen((IosNotificationSettings setting) {
+      print('Ios Setting Registed');
+    });
+    firebaseMessageing.getToken().then((token) {
+      updateToken(token);
+    });
+  }
+
+  void updateToken(String token) {
+    print(token);
+    textValue = token;
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.blue[400],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[400],
         title: Text('Show News List'),
       ),
       body: new Center(
         child: Container(
-          child: Text('Show News List'),
+          child: Text(textValue),
         ),
       ),
     );
