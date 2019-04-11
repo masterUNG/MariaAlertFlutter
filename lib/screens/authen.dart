55e0634a41fac95a4850d58f8d703ab8d5f9a025
masterUNG <phrombutr@gmail.com>
2019-04-11 09:11:51 +0700

    update

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,15 +6,70 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  bool statusRemember = false;
+
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
+  }
+
+  Widget emailTextFromField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Email Address:',
+          hintText: 'you@email.com',
+          labelStyle:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+    );
+  }
+
+  Widget passwordTextFromField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Password:',
+          hintText: 'Your Password',
+          labelStyle:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+    );
+  }
+
+  Widget rememberCheckBox() {
+    return CheckboxListTile(
+      controlAffinity: ListTileControlAffinity.leading,
+      title: Text(
+        'Remember Me',
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      value: statusRemember,
+      onChanged: (bool value) {
+        onRememberCheck(value);
+      },
+    );
+  }
+
+  void onRememberCheck(bool value) {
+    setState(() {
+      statusRemember = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Authentication'),
-      ),
-      body: new Center(
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0, left: 50.0, right: 50.0),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.blue[700], Colors.blue[50]],
+                begin: Alignment(-1, -1))),
         child: Container(
-          child: Text('Authen'),
+          child: Column(
+            children: <Widget>[
+              showLogo(),
+              emailTextFromField(),
+              passwordTextFromField(),
+              rememberCheckBox()
+            ],
+          ),
         ),
       ),
     );
