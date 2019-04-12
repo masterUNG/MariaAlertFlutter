24378ad41b4ac775dbe306bdb3b8893091c9cccc
masterUNG <phrombutr@gmail.com>
2019-04-12 07:42:51 +0700

    Validate OK

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,29 +6,50 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  final formKey = GlobalKey<FormState>();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
   bool statusRemember = false;
+  String user, password;
+
+  String titleUser = 'ลงชื่อเข้าใช้งาน :';
+  String titlePassword = 'รหัส :';
+  String titleRemember = 'จดจำ ชื่อผู้ใช้งาน และ รหัส';
+  String titleHaveSpace = 'มีช่องว่าง';
+  String messageHaveSpace = 'กรุณากรอกข้อมูลทุกช่อง คะ';
 
   Widget showLogo() {
     return Image.asset('images/logo1.png');
   }
 
-  Widget emailTextFromField() {
+  Widget userTextFromField() {
     return TextFormField(
       decoration: InputDecoration(
-          labelText: 'Email Address:',
-          hintText: 'you@email.com',
+          hintStyle: TextStyle(color: Colors.white),
+          labelText: titleUser,
+          hintText: 'Your User',
           labelStyle:
               TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+      validator: (String value) {
+        if (value.length == 0) {
+          return messageHaveSpace;
+        }
+      },
     );
   }
 
   Widget passwordTextFromField() {
     return TextFormField(
       decoration: InputDecoration(
-          labelText: 'Password:',
+          hintStyle: TextStyle(color: Colors.white),
+          labelText: titlePassword,
           hintText: 'Your Password',
           labelStyle:
-              TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+              TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),validator: (String value) {
+                if (value.length == 0) {
+                  return messageHaveSpace;
+                }
+              },
     );
   }
 
@@ -36,7 +57,7 @@ class _AuthenState extends State<Authen> {
     return CheckboxListTile(
       controlAffinity: ListTileControlAffinity.leading,
       title: Text(
-        'Remember Me',
+        titleRemember,
         style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       ),
       value: statusRemember,
@@ -58,45 +79,52 @@ class _AuthenState extends State<Authen> {
       shape: new RoundedRectangleBorder(
           borderRadius: new BorderRadius.circular(30.0)),
       child: Text(
-        'Login',
+        titleUser,
         style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       ),
-      onPressed: () {},
+      onPressed: () {
+        print('You Click Login');
+        print(formKey.currentState.validate());
+        if (formKey.currentState.validate()) {}
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(resizeToAvoidBottomPadding: false,
-      body: Container(
-        padding: EdgeInsets.only(top: 50.0, left: 50.0, right: 50.0),
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.blue[700], Colors.blue[50]],
-                begin: Alignment(-1, -1))),
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                constraints: BoxConstraints.expand(width: 150, height: 150),
-                child: showLogo(),
-              ),
-              emailTextFromField(),
-              passwordTextFromField(),
-              rememberCheckBox(),
-              Row(
+    return Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Form(
+          key: formKey,
+          child: Container(
+            padding: EdgeInsets.only(top: 50.0, left: 50.0, right: 50.0),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.blue[700], Colors.blue[50]],
+                    begin: Alignment(-1, -1))),
+            child: Container(
+              child: Column(
                 children: <Widget>[
-                  new Expanded(
-                    child: Container(
-                      child: loginButton(),
-                    ),
+                  Container(
+                    constraints: BoxConstraints.expand(width: 150, height: 150),
+                    child: showLogo(),
+                  ),
+                  userTextFromField(),
+                  passwordTextFromField(),
+                  rememberCheckBox(),
+                  Row(
+                    children: <Widget>[
+                      new Expanded(
+                        child: Container(
+                          child: loginButton(),
+                        ),
+                      )
+                    ],
                   )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
