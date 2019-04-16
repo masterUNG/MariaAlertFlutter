16f01522e960bb50bcc93925e0237d4f57606f7a
masterUNG <phrombutr@gmail.com>
2019-04-16 12:37:14 +0700

    update 16April

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'screens/authen.dart';
 import 'screens/show_news_list.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(App());
+void main() => runApp(MyApp());
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations(
