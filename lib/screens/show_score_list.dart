71d6080eeba1fd0749fda42f3ae15b97c8086cf6
masterUNG <phrombutr@gmail.com>
2019-07-07 08:33:56 +0700

    Show Score

--- lib/screens/show_score_list.dart
+++ lib/screens/show_score_list.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:mariealert/listviews/score_listview.dart';
+import 'dart:convert';
+
+import 'package:mariealert/models/score_model.dart';
 
 class ShowScoreList extends StatefulWidget {
   final String idCode;
@@ -9,13 +14,37 @@ class ShowScoreList extends StatefulWidget {
 }
 
 class _ShowScoreListState extends State<ShowScoreList> {
+
+  List<ScoreModel> scoreModels = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    String idcode = widget.idCode;
+    String urlString = 'http://tscore.ms.ac.th/App/getScoreWhereIdCode.php?isAdd=true&idcode=$idcode';
+    var response = await get(urlString);
+    var result = json.decode(response.body);
+    print('result = $result');
+    for (var objData in result) {
+      ScoreModel scoreModel = ScoreModel.fromJson(objData);
+      setState(() {
+        scoreModels.add(scoreModel);
+      });
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Show Score'),
       ),
-      body: Text('body'),
+      body: ScoreListView(scoreModels),
     );
   }
 }
